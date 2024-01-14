9e41d6aa498459f5b2a95afad6f0260f2c86881a
Rocio <144937216+Rociosantos18@users.noreply.github.com>
2024-01-14 15:20:45 +0000

    first commit

---
+++ src/js/component/Light.jsx
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+
+
+//create your first component
+
+const Light = () => {
+
+    const [color, setColor] = useState("red");
+    
+
+    
+    return (
+        <div className="traficLight text-center">
+
+            <div  onClick={() => setColor ("red")} className={"light red" + ((color === "red") ? " glow" : "")}></div>
+            <div onClick={() => setColor ("yellow")} className={"light yellow" + ((color === "yellow") ? " glow" : "")}></div>
+            <div onClick={() => setColor ("green")} className={"light green" + ((color === "green") ? " glow" : "")}></div>
+        </div>
+    );
+};
+
+export default Light;
